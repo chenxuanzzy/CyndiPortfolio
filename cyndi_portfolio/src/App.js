42d6364398ef97e3ea5bae71da51fb3df5dfc5dc
chenxsuanzzy <chenxsuan019@gmail.com>
2023-01-09 17:32:46 +0800

    git init

---
+++ cyndi_portfolio/src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from "react";
+
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import About from "./Components/About";
+import Experience from "./Components/Experience";
+import Contact from "./Components/Contact";
+import Project from "./Components/Project";
+
+import "./App.css";
+
+const App = () => {
+  const [resumeData, setResumeData] = useState({});
+
+  useEffect(() => {
+    fetch("/resumeData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setResumeData(data);
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      <Header data={resumeData.main} />
+      <About data={resumeData.main} />
+      <Experience data={resumeData.resume} />
+      <Project data={resumeData.portfolio} />
+      <Contact data={resumeData.main} />
+      <Footer data={resumeData.main} />
+    </div>
+  );
+};
+
+export default App;
---
+++ cyndi_portfolio/src/Components/Footer.js
@@ -0,0 +1,34 @@
+const Footer = ({ data }) => {
+  if (data) {
+    var networks = data.social.map((network) => {
+      return (
+        <li key={network.name}>
+          <a href={network.url}>
+            <i className={network.className}></i>
+          </a>
+        </li>
+      );
+    });
+  }
+  return (
+    <footer>
+      <div className="row">
+        <div className="twelve columns">
+          <ul className="social-links">{networks}</ul>
+          <ul className="copyright">
+            <li>
+              <p>Made by Cyndi Chao © {new Date().getFullYear()}</p>
+            </li>
+          </ul>
+        </div>
+        <div id="go-top">
+          <a className="smoothscroll" title="Back to Top" href="#home">
+            <i className="icon-up-open"></i>
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
